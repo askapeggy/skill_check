feade2be97dda5799c4f3dd38afbf2ca93d9a042
askapeggy <aska.aska@msa.hinet.net>
2024-12-30 14:06:15 +0800

    網頁內容

--- 02/admin.php
+++ 02/admin.php
@@ -1,5 +1,4 @@
-﻿<?php include_once "./api/db.php";?>
-<!DOCTYPE html
+﻿<!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -20,10 +19,7 @@
     </div>
     <div id="all">
         <div id="title">
-            <?=date("m 月 d 號 l");?> | 今日瀏覽: <?=$Total->find(['date'=>date("Y-m-d")])['total'];?>
-            | 累積瀏覽: <?= $Total->sum('total');?>
-            <a href="index.php" style="float:right">回首頁</a>
-        </div>
+            <?=date("m 月 d 號 l");?> | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
         <div id="title2">
             <a href="index.php" title="健康促進網-回首頁">
                 <img src="icon/02B01.jpg" alt="健康促進網-回首頁">
@@ -31,11 +27,11 @@
         </div>
         <div id="mm">
             <div class="hal" id="lef">
+                <a class="blo" href="?do=acc">帳號管理</a>
                 <a class="blo" href="?do=po">分類網誌</a>
-                <a class="blo" href="?do=news">最新文章</a>
-                <a class="blo" href="?do=pop">人氣文章</a>
-                <a class="blo" href="?do=know">講座訊息</a>
-                <a class="blo" href="?do=que">問卷調查</a>
+                <a class="blo" href="?do=news">最新文章管理</a>
+                <a class="blo" href="?do=know">講座管理</a>
+                <a class="blo" href="?do=que">問卷管理</a>
             </div>
             <div class="hal" id="main">
                 <div>
@@ -49,13 +45,13 @@
 
                 <?php
 					$do = $_GET['do']??'main';
-					$file = "front/".$do.".php";
+					$file = "back/".$do.".php";
 					if(file_exists($file))
 					{
 						include $file;
 					}else
 					{
-						include "/front/main.php";
+						include "/back/main.php";
 					}
 				?>
             </div>
@@ -66,6 +62,7 @@
             服務信箱：health@test.labor.gov.tw<img src="./image/02B02.jpg" width="45">
         </div>
     </div>
+
 </body>
 
 </html>
