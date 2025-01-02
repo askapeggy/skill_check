0a6d566f5e4d0c327e810af45ebefa930fb8eb91
askapeggy <aska.aska@msa.hinet.net>
2025-01-02 09:12:52 +0800

    登入比對和跳頁和訊息顯示

--- 02/api/chk_acc.php
+++ 02/api/chk_acc.php
@@ -1,5 +1,3 @@
 <?php include_once "db.php";
-//$acc=$_GET['acc'];
-//echo $User->count(['acc'=>$acc]);
 echo $User->count($_GET);
 ?>
--- 02/front/login.php
+++ 02/front/login.php
@@ -10,7 +10,8 @@
             <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
-            <td><input type="button" value="登入" onclick="Login()"> <input type="reset" value="清除"></td>
+            <td><input type="button" value="登入" onclick="Login()"> <input type="reset" value="清除" onclick="resetForm()">
+            </td>
             <td>
                 <a href="?do=forgot">忘記密碼</a>
                 <a href="?do=reg">尚未註冊</a>
@@ -26,17 +27,38 @@ function Login() {
         pw: $("#pw").val(),
     };
     if (user.acc.lenght != 0 && user.pw.lenght != "") {
-        //有資料
-        $.post("./api/login.php", user, (res) => {
-            console.log("login =>", res);
+        $.get("./api/chk_acc.php", {
+            acc: user.acc
+        }, (res) => {
+            console.log("chk acc=>", res);
             if (parseInt(res) == 0) {
-                alert("請輸入正確帳號和密碼");
+                alert("查無帳號");
+                resetForm();
             } else {
-                alert("恭喜登入");
+                //有資料
+                $.post("./api/chk_pw.php", user, (res) => {
+                    console.log("chk_pw =>", res);
+                    if (parseInt(res) == 0) {
+                        alert("密碼錯誤");
+                        resetForm();
+                    } else {
+                        alert("恭喜登入");
+                        if (user.acc == "admin") {
+                            location.href = "./admin.php";
+                        } else {
+                            location.href = "./index.php";
+                        }
+                    }
+                })
             }
         })
     } else {
         alert("請輸入帳號和密碼");
     }
+}
+
+function resetForm() {
+    $("#acc").val("");
+    $("#pw").val("");
 }
 </script>
