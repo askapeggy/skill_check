addf17d5a3b97021002f8c1996cbfa121aa6f7b5
askapeggy <aska.aska@msa.hinet.net>
2025-01-03 15:13:11 +0800

    按讚

--- 02/api/like.php
+++ 02/api/like.php
@@ -2,11 +2,15 @@
     $id = $_POST['id'];
     $user = $_SESSION['user'];
     $count = $Log->count(['news'=>$id, 'user'=>$user]);
+    $mynews = $News->find($id);
     if($count == 0)
     {//有按讚
         $Log->save(['news'=>$id, 'user'=>$user]);
+        $mynews['likes']++;
     }else
     {//收回讚
         $Log->del(['news'=>$id, 'user'=>$user]);
+        $mynews['likes']--;
     }
-?>
+    $News->save($mynews);
+    ?>
--- 02/front/news.php
+++ 02/front/news.php
@@ -1,3 +1,8 @@
+<style>
+.detail {
+    display: none;
+}
+</style>
 <fieldset>
     <legend>目前位置:首頁 > 最新文章區 </legend>
 
@@ -16,8 +21,11 @@
             $rows = $News->all(['sh'=>1], " Limit $start, $div");
             foreach($rows as $row):?>
         <tr>
-            <td><?=$row['title'];?></td>
-            <td><?=mb_substr($row['news'],0,25);?>...</td>
+            <td class="row-title"><?=$row['title'];?></td>
+            <td>
+                <span class="title"><?=mb_substr($row['news'],0,25);?>...</span>
+                <span class="detail"><?=nl2br($row['news']);?></span>
+            </td>
             <td>
                 <?php
                     if(isset($_SESSION['user']))
@@ -71,5 +79,9 @@ $(".like").on("click", function() {
         }
     });
 
+})
+
+$(".row-title").on("click", function() {
+    $(this).next().children(".title, .detail ").toggle();
 })
 </script>
